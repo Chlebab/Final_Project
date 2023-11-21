d1cfbf093c23ecc1f9df93d9dc66ecacae2eadc0
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-11-21 13:53:37 +0000

    done timer

--- Menus/MainMenu.gd
+++ Menus/MainMenu.gd
@@ -1,12 +1,17 @@
 extends Control
 
 func _ready():
-	$VBoxContainer/Start.grab_focus()
+	$VBoxContainer/StartButton.grab_focus()
 
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://LevelOne.tscn")
 
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://Menus/Options.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+
