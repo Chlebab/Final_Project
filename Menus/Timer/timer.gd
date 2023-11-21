39c23b4838c02e5ed1db6fa94bddfcfb145d7d27
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-11-21 23:38:28 +0000

    showing final time and score

--- Menus/Timer/timer.gd
+++ Menus/Timer/timer.gd
@@ -1,33 +1,33 @@
 extends Panel
 
-var time: float = 150.0
+
 var minutes: int = 0 
 var seconds: int = 0
 var msecs: int = 0
 var flash_timer: float = 0
 var flash_visible: bool = false
 var flash_frequency: float = 0.5 
-var points: int = 0
+
 
 func _process(delta) -> void:
-	time -= delta
-	if time <= 0:
-		time = 0
+	Global.time -= delta
+	if Global.time <= 0:
+		Global.time = 0
 	#	if points > 10000:
 	#		get_tree().change_scene_to_file(LevelTwo.tscn)
 	#	else:
 	#			get_tree().change_scene_to_file(GameOver.tscn)
 		stop()
 		
-	msecs = fmod(time, 1) * 100
-	seconds = fmod(time, 60)
-	minutes = fmod(time, 3600) / 60
+	msecs = fmod(Global.time, 1) * 100
+	seconds = fmod(Global.time, 60)
+	minutes = fmod(Global.time, 3600) / 60
 	$Minutes.text = "%02d:" % minutes
 	$Seconds.text = "%02d:" % seconds
 	$Miliseconds.text = "%02d" % msecs
 	
-	if time <= 5:
-		flash_timer = fmod(time, flash_frequency * 2)
+	if Global.time <= 5:
+		flash_timer = fmod(Global.time, flash_frequency * 2)
 		flash_visible = flash_timer < flash_frequency
 		$Minutes.visible = flash_visible
 		$Seconds.visible = flash_visible
@@ -41,7 +41,7 @@ func _process(delta) -> void:
 		$Miliseconds.modulate = Color(1, 1, 1)
 	
 	
-	if time <= 10:
+	if Global.time <= 10:
 		$Minutes.modulate = Color(1, 0, 0)
 		$Seconds.modulate = Color(1, 0, 0)
 		$Miliseconds.modulate = Color(1, 0, 0)
@@ -52,7 +52,7 @@ func _process(delta) -> void:
 	
 func stop() -> void:
 	set_process(false)
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://Menus/ResultPage.tscn")
 
 func get_time_formatted() -> String:
 	return "%02d:%02d.%03d" % [minutes, seconds, msecs]
