34be64560050750d85c36e2f039bed5014e27087
Chlebab <miksob@gmail.com>
2023-11-22 14:40:13 +0000

    test

--- Player/Player.gd
+++ Player/Player.gd
@@ -44,4 +44,45 @@ func empty_inventory():
 func _on_pick_up_cart_drop_off_inv_at_cart():
 	print("You have reached the drop off point. Your inventory has been packed to the cart.")
 	empty_inventory()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("r"):
+		use_egg_item()
+#	elif Input.is_action_just_pressed("2"):
+#		use_item(2)
+#	elif Input.is_action_just_pressed("3"):
+#		use_item(3)
+#	use_item()
+
+func reduce_amount_by_1(i):
+	if inv.slots[i].amount > 1:
+		inv.slots[i].amount -= 1
+	else:
+		inv.slots[i].item = null
+	update_slots.emit()
+
+func use_egg_item():
+	var egg_slot = -1
+	print("r pressed, use egg function triggered")
+	for i in inv.slots.size():
+		print("comapring egg resource <Resource#-9223372009994451777> to : ", str(inv.slots[i].item))
+		if str(inv.slots[i].item) == "<Resource#-9223372009927342909>":
+			egg_slot = i
+			break
+	
+	if egg_slot != -1:
+		print("You got yourself an egg in slot i ", inv.slots[egg_slot].item)
+		reduce_amount_by_1(egg_slot)
+	else:
+		print("No egg found in the inventory")
+#	var eggItemSlot = inv.slots.filter(func(egg): return egg.inv.item == inv.item)
+#	if !eggItemSlot.is_empty():
+#		eggItemSlot[0].amount += 1
+#	if str(inv.slots[i-1].item) == "<Resource#-9223372009994451777>":
+#		print("You got yourself an egg in slot 1 ", inv.slots[0].item)
+#		print("inv.slots[0].amount: ", inv.slots[0].amount)
+#		reduce_amount_by_1()
+#		print("You have just used an item")		
+#	else:
+#		print("No item detected in slot 0", inv.slots[0].item)
 	
