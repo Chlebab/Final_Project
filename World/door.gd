c654cf543e0e2c70c7506e58c012656145c3a4e9
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-11-24 12:59:02 +0000

    doors now opening with key

--- World/door.gd
+++ World/door.gd
@@ -1,27 +1,25 @@
 extends StaticBody2D
 
+@export var inv: Inventory
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_door_area_body_entered(body):
+	print("Player entered")
 	if body.get_name() == "Player":
-		print("Player entered")
-		$Close.hide()
-		$Open.show()
-		$Open.play
+		for i in body.inv.slots.size():
+			if body.inv.slots[i].item and str(body.inv.slots[i].item.name) == "Key":
+				$Close.hide()
+				$Open.show()
+				$Open.play
 	
-
-
 func _on_door_area_body_exited(body):
+	print("Player exited")
 	if body.get_name() == "Player":
-		print("Player exited")
 		$Open.hide()
 		$Close.show()
 		$Close.play
