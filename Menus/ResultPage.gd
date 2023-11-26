6d3a6f6fc7e2b46501910ed0da3610c6f835fee6
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-11-26 23:48:43 +0000

    added chest

--- Menus/ResultPage.gd
+++ Menus/ResultPage.gd
@@ -9,9 +9,20 @@ func _process(delta):
 func _ready():
 	$PlayAgain.grab_focus()
 	$Points.text = str("POINTS  ", Global.points)
+	await get_tree().create_timer(1).timeout
+	$Points.show()
 	$Time.text = str("TIME  ", time_taken, "secs")
+	await get_tree().create_timer(1).timeout
+	$Time.show()
 	score = Global.points + (10*Global.time)
 	$Score.text = str("SCORE  ", score)
+	await get_tree().create_timer(1).timeout
+	$Score.show()
+	await get_tree().create_timer(1).timeout
+	$PlayAgain.show()
+	$Quit.show()
+	
+	
 
 func _on_play_again_pressed():
 	get_tree().change_scene_to_file("res://Levels/DungeonLevel.tscn")
