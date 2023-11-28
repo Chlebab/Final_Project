f4c0012897bb8e45cede86f632931dcd93e7a7d7
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-11-28 16:31:45 +0000

    transitions done up to level three

--- Levels/EndLevelTwo.gd
+++ Levels/EndLevelTwo.gd
@@ -9,4 +9,4 @@ func _on_end_level_area_body_entered(body):
 		body.empty_inventory()
 		get_parent().get_node("Transition").play("fade_out")
 		await get_tree().create_timer(3).timeout
-		get_tree().change_scene_to_file("res://Levels/LevelTwo.tscn")
+		get_tree().change_scene_to_file("res://Levels/LevelThree.tscn")
