601fb17a92ca9b9d0b5135f741cf04db2f5555c1
Ben Counsell <bencounsell@hotmail.co.uk>
2023-11-29 10:47:00 +0000

    put a nice weak candle in the cell in level 2

--- World/FlickeringLight.gd
+++ World/FlickeringLight.gd
@@ -3,9 +3,10 @@ extends PointLight2D
 var random = RandomNumberGenerator.new()
 
 func _ready():
-	get_parent().play()
+	if get_parent().has_method("play"):
+		get_parent().play()
 	random.randomize()
 	while 1 == 1:
-		await get_tree().create_timer(randf_range(0.06, 0.14)).timeout
-		energy = randf_range(0.66, 0.85)
-		texture_scale = randf_range(13.0,14.7)
+		await get_tree().create_timer(randf_range(0.06, 0.12)).timeout
+		energy = randf_range(0.76, 0.85)
+		texture_scale = randf_range(13.8,14.7)
