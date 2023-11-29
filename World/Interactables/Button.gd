a2cac951709eb43c8f3ec8f21fcff63220451d21
Ben Counsell <bencounsell@hotmail.co.uk>
2023-11-29 10:20:22 +0000

    little fixes

--- World/Interactables/Button.gd
+++ World/Interactables/Button.gd
@@ -21,12 +21,13 @@ func _on_button_area_body_entered(body):
 	if body.get_name() == "Player":
 		var inv_msg_label = body.get_node("InvMsg")
 		var inv_msg_timer = body.get_node("InvMsgTimer")
-		inv_msg_label.text = "[O] to press button!"
+		inv_msg_label.text = "[E] to press button!"
 		inv_msg_timer.start()
 		player_in_area = true
 		print("player in button")
 
 
-func _on_button_area_body_exited():
-	player_in_area = false
-	print("player left button")
+func _on_button_area_body_exited(body):
+	if body.get_name() == "Player":
+		player_in_area = false
+		print("player left button")
